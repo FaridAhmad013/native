e347aced780fd25cdea1d20ee1a63480c348e4e7
FaridAhmad013 <lilisrohanah13@gmail.com>
2021-09-11 10:42:26 +0700

    Memperbaiki rumus segitiga

--- oop/Segitiga.php
+++ oop/Segitiga.php
@@ -4,16 +4,16 @@
     {
         public $alas;
         public $tinggi;
-        public $setengah = 0.5;
+        public $a,$b,$c;
 
         public function luasSegitiga()
         {
-            return $this->setengah*($this->alas*$this->tinggi);
+            return ($this->alas*$this->tinggi)/2;
         }
 
         public function kelilingSegitiga()
         {
-            return $this->alas*$this->tinggi;
+            return $this->a*$this->b*$this->c;
         }
 
     }
@@ -21,8 +21,14 @@
     $segitiga = new Segitiga;
     $segitiga->alas = 10;
     $segitiga->tinggi = 10;
+    $segitiga->a =10;
+    $segitiga->b = 10;
+    $segitiga->c = 10;
 
     echo "Alas : {$segitiga->alas} <br>";
     echo "Tinggi : {$segitiga->tinggi} <br>";
+    echo "a : {$segitiga->a} <br>";
+    echo "b : {$segitiga->b} <br>";
+    echo "c : {$segitiga->c} <br>";
     echo "Luas Segitiga : ".$segitiga->luasSegitiga();
     echo "<br> Keliling Segitiga : ".$segitiga->kelilingSegitiga();
