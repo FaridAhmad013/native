14a5500714838e8914f2e80dc5f727ce5e187553
FaridAhmad013 <lilisrohanah13@gmail.com>
2021-09-13 16:17:43 +0700

    Memperbaiki file Kucing

--- oop/Kucing.php
+++ oop/Kucing.php
@@ -6,21 +6,28 @@
         public $jenisBulu = "Panjang";
         public $makananFavorit = "Ikan";
 
-        public function bersuara(){
+        public function __construct($warna)
+        {
+            $this->warna = $warna;
+        }
+
+        public function bersuara()
+        {
             return "Nyan";
         }
 
-        public function berburu(){
+        public function berburu()
+        {
             return "Berburu ikan";
         }
 
     }
 
-    $kucing1 = new Kucing;
+    $kucing1 = new Kucing("hitam");
     echo "Warna Kucing : ".$kucing1->warna."<br>";
     echo "Suara kucing : ".$kucing1->bersuara();
 
     echo "<hr>";
-    $kucing1 = new Kucing;
-    echo "Warna Kucing : ".$kucing1->warna = "oren"."<br>";
+    $kucing1 = new Kucing("blanc");
+    echo "Warna Kucing : ".$kucing1->warna."<br>";
     echo "Suara kucing : ".$kucing1->bersuara();
